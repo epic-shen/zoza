3467941e8272afdb84d6f72936edbc10a872d790
epic-shen <93494553+epic-shen@users.noreply.github.com>
2022-04-05 17:14:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,165 +1,268 @@
-//  zoza Module
+//  zoza module
 const { Telegraf } = require('telegraf-develop')
 
 
-class zoza {
-	constructor (token) {
-		this.token = token
-		this.bot = new Telegraf(this.token)
-	}
+class ZozaBot {
+  constructor (token) {
+    this.token = token
+    this.bot = new Telegraf(this.token)
+  }
 
 	
-  start (ctx) {
-	  this.bot.start((ctx))
+  start(ctx) {
+    this.bot.start((ctx))
   }
 	
-	command (name, ctx) {
-	  this.bot.hears(name, (ctx))
+  command(name, ctx) {
+    this.bot.hears(name, (ctx))
   }
 	
-  send (ctx, text, extra = null) {
-		ctx.reply(text, extra)
-	} 
-
-	leave (ctx) {
-	  this.bot.telegram.leaveChat(ctx.message.chat.id)
-	}
-
-  event (type, ctx) {
+  event(type, ctx) {
     if (type == 'message') {
-			this.bot.on(type, ctx)
-		}
-		if (type == 'text') {
-			this.bot.on(type, ctx)
-		}
-		if (type == 'voice') {
-			this.bot.on(type, ctx)
-		}
-		if (type == 'video') {
-			this.bot.on(type, ctx)
-		}
-		if (type == 'audio') {
-			this.bot.on(type, ctx)
-		}
-		if (type == 'document') {
-			this.bot.on(type, ctx)
-		}
-		if (type == 'photo') {
-			this.bot.on(type, ctx)
-		}
-		if (type == 'contact') {
-			this.bot.on(type, ctx)
-		}
-		if (type == 'sticker') {
-			this.bot.on(type, ctx)
-		}
-		if (type == 'gif') {
-			this.bot.on('animation', ctx)
-		}
-		if (type == 'dice') {
-			this.bot.on(type, ctx)
-		}
-		if (type == 'voice_chat_started') {
-			this.bot.on(type, ctx)
-		}
-		if (type == 'voice_chat_ended') {
-			this.bot.on(type, ctx)
-		}
-		if (type == 'voice_chat_planned') {
-			this.bot.on('voice_chat_scheduled', ctx)
-		}
-		if (type == 'voice_chat_invite') {
-			this.bot.on('voice_chat_participants_invited', ctx)
-		}
-		if (type == 'supergroup_created') {
-			this.bot.on('supergroup_chat_created', ctx)
-		}
-		if (type == 'forwarded_message') {
-			this.bot.on('forward_date', ctx)
-		}
-		if (type == 'pinned_message') {
-			this.bot.on(type, ctx)
-		}
-		if (type == 'edited_message') {
-			this.bot.on(type, ctx)
-		}
-		if (type == 'member_join') {
-			this.bot.on('new_chat_members', ctx)
-		}
-		if (type == 'member_leave') {
-			this.bot.on('left_chat_member', ctx)
-		}
-		if (type == 'poll_answer') {
-			this.bot.on(type, ctx)
-				}
-		if (type == 'anon_poll_answer') {
-			this.bot.on('poll', ctx)
-		}
-		if (type == 'channel_post') {
-			this.bot.on(type, ctx)
-		}
-		if (type == 'edited_channel_post') {
-			this.bot.on(type, ctx)
-		}
-		if (type == 'member_join_request') {
-			this.bot.on('chat_join_request', ctx)
-		}
-		if (type == 'new_chat_title') {
-			this.bot.on(type, ctx)
-		}
-		if (type == 'new_chat_photo') {
-			this.bot.on(type, ctx)
-		}
-		if (type == 'chat_photo_deleted') {
-			this.bot.on('delete_chat_photo', ctx)
-		}
-	}
+      this.bot.on(type, ctx)
+    }
+    if (type == 'text') {
+      this.bot.on(type, ctx)
+    }
+  		if (type == 'voice') {
+      this.bot.on(type, ctx)
+    }
+	  	if (type == 'video') {
+      this.bot.on(type, ctx)
+    }
+  		if (type == 'audio') {
+      this.bot.on(type, ctx)
+    }
+	  	if (type == 'document') {
+      this.bot.on(type, ctx)
+    }
+    if (type == 'photo') {
+			  this.bot.on(type, ctx)
+		  }
+    if (type == 'contact') {
+			  this.bot.on(type, ctx)
+    }
+    if (type == 'sticker') {
+      this.bot.on(type, ctx)
+    }
+    if (type == 'gif') {
+			  this.bot.on('animation', ctx)
+			}
+    if (type == 'dice') {
+      this.bot.on(type, ctx)
+    }
+    if (type == 'voice_chat_started') {
+      this.bot.on(type, ctx)
+    }
+    if (type == 'voice_chat_ended') {
+      this.bot.on(type, ctx)
+    }
+    if (type == 'voice_chat_planned') {
+      this.bot.on('voice_chat_scheduled', ctx)
+    }
+    if (type == 'voice_chat_invite') {
+      this.bot.on('voice_chat_participants_invited', ctx)
+    }
+    if (type == 'supergroup_created') {
+      this.bot.on('supergroup_chat_created', ctx)
+    }
+    if (type == 'forwarded_message') {
+      this.bot.on('forward_date', ctx)
+    }
+    if (type == 'pinned_message') {
+      this.bot.on(type, ctx)
+    }
+    if (type == 'edited_message') {
+      this.bot.on(type, ctx)
+    }
+    if (type == 'member_join') {
+      this.bot.on('new_chat_members', ctx)
+    }
+    if (type == 'member_leave') {
+      this.bot.on('left_chat_member', ctx)
+    }
+    if (type == 'poll_answer') {
+      this.bot.on(type, ctx)
+    }
+    if (type == 'anon_poll_answer') {
+      this.bot.on('poll', ctx)
+    }
+    if (type == 'channel_post') {
+      this.bot.on(type, ctx)
+    }
+    if (type == 'edited_channel_post') {
+      this.bot.on(type, ctx)
+    }
+    if (type == 'member_join_request') {
+      this.bot.on('chat_join_request', ctx)
+    }
+    if (type == 'new_chat_title') {
+      this.bot.on(type, ctx)
+    }
+    if (type == 'new_chat_photo') {
+      this.bot.on(type, ctx)
+    }
+    if (type == 'chat_photo_deleted') {
+      this.bot.on('delete_chat_photo', ctx)
+			}
+  }
 	
-  message (ctx, key) {
-		if (key = 'id') {
-			return ctx.message.message_id
-		}
-		if (key = 'date') {
-			return ctx.message.date
-		}
-		if (key = 'text') {
-			return ctx.message.text
-		}
-		if (key = 'user_id') {
-			return ctx.message.from.id
-		}
-		if (key = 'user_first_name') {
-			return ctx.message.from.first_name
-		}
-		if (key = 'user_last_name') {
-			return ctx.message.from.last_name
-		}
-		if (key = 'user_username') {
-			return ctx.message.from.username
-		}
-		if (key = 'user_is_bot') {
-			return ctx.message.from.is_bot
-		}
-		if (key = 'chat_id') {
-			return ctx.message.chat.id
-		}
-		if (key = 'chat_title') {
-			return ctx.message.chat.title
-		}
-		if (key = 'chat_type') {
-			return ctx.message.chat.type
-		}
-	}
-	
-	run() {
-	  this.bot.launch()
-    console.log("\x1b[32m", "Бот успешно запущен!")
-	}
+  run() {
+    this.bot.launch()
+    console.log("\x1b[32m", "\x1b[2m", "Бот успешно запущен!")
+  }
 }
+
+
+
+
+
+
+
+
+class zoza {
+  constructor(ctx) {
+    this.ctx = ctx
+    this.msg = {
+    		"id": this.ctx.message.message_id,
+    		"text": this.ctx.message.text,
+    		"date": this.ctx.message.date,
+    		"user_id": this.ctx.message.from.id,
+    		"name": this.ctx.message.from.first_name,
+    		"surname": this.ctx.message.from.last_name,
+    		"username": this.ctx.message.from.username,
+    		"is_bot": this.ctx.message.from.is_bot,
+    		"chat_id": this.ctx.message.chat.id,
+    		"chat_title": this.ctx.message.chat.title,
+    		"chat_type": this.ctx.message.chat.type
+    }
+  }
+
+
+  // ctx.message
+  get id() {
+    return this.msg['id']
+  }
+
+  get text() {
+    return this.msg['text']
+  }
+
+  get date() {
+    return this.msg['date']
+  }
+  
+  get user_id() {
+    return this.msg['user_id']
+  }
+  
+  get user_name() {
+    return this.msg['name']
+  }
+  
+  get user_surname() {
+    return this.msg['surname']
+  }
+  
+  get user_username() {
+    return this.msg['username']
+  }
+  
+  get user_is_bot() {
+    return this.msg['is_bot']
+  }
+  
+  get chat_id() {
+    return this.msg['chat_id']
+  }
+  
+  get chat_title() {
+    return this.msg['chat_title']
+  }
+  
+  get chat_type() {
+    return this.msg['chat_type']
+  }
+
+
+  // User Tag
+  user_tag(type = "html") {
+    if (type == 'HTML' || type == 'html' || type == 'Html') {
+	   	return `<a href="tg://user?id=${this.msg['user_id']}">${this.msg['name']}</a>`
+    } 
+    if (type == 'Markdown' || type == 'markdown' || type == 'md') {
+	   	return `[${this.msg['user_id']}](tg://user?id=${this.msg['name']})`
+    } 
+  }
+  
+  custom_tag(id, name, type = "html") {
+    if (type == 'HTML' || type == 'html' || type == 'Html') {
+	   	return `<a href="tg://user?id=${id}">${name}</a>`
+    } 
+    if (type == 'Markdown' || type == 'markdown' || type == 'md') {
+	   	return `[${name}](tg://user?id=${id})`
+    } 
+  }
+  
+  
+  send(text, type = "none", extra = null) {
+    if (type == "none") {
+      this.ctx.reply(text, extra)
+    }
+    if (type == 'HTML' || type == 'html' || type == 'Html') {
+      this.ctx.replyWithHTML(text, extra)
+    } 
+    if (type == 'Markdown' || type == 'markdown' || type == 'md') {
+      this.ctx.replyWithMarkdownV2(text, extra)
+    }
+  }
+
+  dm(text, user_id = this.msg['user_id'], extra = null) {
+    this.ctx.telegram.sendMessage(user_id, text, extra)
+  }
+
+  leave(chat_id = this.msg['chat_id']) {
+    this.ctx.telegram.leaveChat(chat_id)
+  }
+  
+  // Pinning
+  pin(message_id = this.msg['id'], chat_id = this.msg['chat_id'], notify = false) {
+  		this.ctx.telegram.pinChatMessage(chat_id, message_id, {
+  				disable_notification: notify
+  		})
+  	}
+  	
+  	unpin(message_id = null, chat_id = this.msg['chat_id']) {
+  		this.ctx.telegram.unpinChatMessage(chat_id, message_id)
+  	}
+  	
+  	unpinAll(chat_id = this.msg['chat_id']) {
+  		this.ctx.telegram.unpinAllChatMessages(chat_id)
+  	}
+  	
+  	ban(user_id, chat_id = this.msg['chat_id'], date = 0, del_messages = true) {
+  		this.ctx.telegram.banChatMember(chat_id, user_id, {
+  				until_date: date,
+  				revoke_messages: del_messages
+  		})
+  	}
+  	
+  unban(user_id, chat_id = this.msg['chat_id']) {
+  		this.ctx.telegram.unbanChatMember(chat_id, user_id)
+  	}
+  	
+  	
+}
+
+
+
 
 module.exports = {
-	zoza
+  ZozaBot,
+  zoza
 }
 
-const module_version = require('./package.json')['version']
+const package = require('./package.json')
+const module_version = package['version']
+
 console.log("\x1b[36m", "\x1b[2m", "zoza  |  " + module_version)
